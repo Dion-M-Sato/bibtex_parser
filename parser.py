e95edf910f82ce799597666f1b72a8a132cf3720
Dion_M_Sato <dion.sato@gmail.com>
2018-12-16 23:22:21 +0900

    next step. REAT API.

---
+++ parser.py
@@ -0,0 +1,22 @@
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.customization import *
+def customizations(record):
+    """Use some functions delivered by the library
+
+    :param record: a record
+    :returns: -- customized record
+    """
+    record = type(record)
+    record = author(record)
+    record = editor(record)
+    record = journal(record)
+    record = keyword(record)
+    return record
+
+with open('sample.bib') as bibtex_file:
+    parser = BibTexParser()
+    parser.customization = customizations
+    bib_database = bibtexparser.load(bibtex_file, parser=parser)
+    print(bib_database.entries)
+

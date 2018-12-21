b34f3f51dacbee8d7d6e5bb1ca2da1ee6224e46c
Dion_M_Sato <dion.sato@gmail.com>
2018-12-21 15:43:55 +0900

    tex ignored.

--- parser.py
+++ parser.py
@@ -1,19 +1,11 @@
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
-from bibtexparser.customization import *
-def customizations(record):
-    """Use some functions delivered by the library
-
-    :param record: a record
-    :returns: -- customized record
-    """
-    record = type(record)
-    record = author(record)
-    record = editor(record)
-    return record
+import json
 
 with open('database_sample.bib') as bibtex_file:
     parser = BibTexParser()
-    parser.customization = customizations
-    bib_database = bibtexparser.load(bibtex_file, parser=parser)
-    print(bib_database.entries)
+    load_database = bibtexparser.load(bibtex_file)
+    #print(bib_database.entries)
+    bib_database = load_database.entries
+    bib_database = json.dumps(bib_database, utf=true)
+    print(bib_database)
